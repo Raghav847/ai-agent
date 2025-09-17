8e6e50af50849776898350e02cb25be3a56c504f
Raghav Khandelwal <raghav@mac.home>
2025-09-16 17:28:00 -0700

    push

--- index.ts
+++ index.ts
@@ -1,4 +1,8 @@
 import 'dotenv/config'
+import { runLLM } from './src/llm'
+import { runAgent } from './src/agent'
+import {z} from 'zod'
+
 
 const userMessage = process.argv[2]
 
@@ -6,3 +10,15 @@ if (!userMessage) {
   console.error('Please provide a message')
   process.exit(1)
 }
+
+const weatherTool = {
+  name: 'get_stuff',
+  description: 'Get the weather',
+  parameters: z.object({
+    reasoning: z.string().describe('why did you pick this tool?'),
+  }),
+}
+
+const response = await runAgent({ userMessage, tools: [weatherTool] })
+
+console.log(response)
